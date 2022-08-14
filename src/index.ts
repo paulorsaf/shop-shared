db7c80052313a75fcd36a9f64eb9c094e6f72912
Paulo Alves <paulorsaf@gmail.com>
2022-08-14 14:04:34 +0400

    Adding index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from './products/commands/update-product-stock/update-product-stock.command';
+export * from './products/commands/update-product-stock/update-product-stock-command.handler';
+export * from './products/commands/update-product-stock/events/product-stock-updated.event';
+export * from './products/entities/product';
+export * from './products/repositories/product.repository';
